d60c17db23bdf92d4f88227a59726da2dcfacfe8
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-01-25 15:48:26 +0100

    Update WASM example

--- nucypher-core-wasm/examples/node/src/main.ts
+++ nucypher-core-wasm/examples/node/src/main.ts
@@ -1,3 +1,22 @@
+import { SecretKey, MessageKit } from "nucypher-core";
+
 export function run() {
-  console.log("Hello world!");
+  // Create a message kit
+  const delegatingSk = SecretKey.random();
+  const delegatingPk = delegatingSk.publicKey();
+  const message = new Uint8Array(Buffer.from("Hello"));
+
+  const messageKit = new MessageKit(delegatingPk, message);
+
+  const decrypted = messageKit.decrypt(delegatingSk);
+  console.assert(
+    decrypted.length === message.length,
+    "Message decrypted correctly"
+  );
+  for (let i = 0; i < message.length; i++) {
+    console.assert(message[i] === decrypted[i], "Message decrypted correctly");
+  }
+  console.log("Success!");
 }
+
+run();
